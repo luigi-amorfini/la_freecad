a249ba09d68571aa30bf96d3d8ee3f5a13b3e578
Luigi Amorfini <107876389+luigi-amorfini@users.noreply.github.com>
2022-07-05 14:24:54 +0200

    Update cube.py

--- Mod/la_freecad/la_freecad/cube.py
+++ Mod/la_freecad/la_freecad/cube.py
@@ -11,10 +11,10 @@ class Cube:
 
     def create(self, length,  width, height):
         cube = App.ActiveDocument.addObject("Part::Box", self.name)
-        cube.Label = self.name        
+        cube.Label = self.name
         cube.Length = length
         cube.Width = width
-        box.Height = height
+        cube.Height = height
         self.obj = cube
 
     def placement(self, placement=App.Placement(App.Vector(0,0,0), App.Rotation(App.Vector(0,0,1),0), App.Vector(0,0,0))):
